f2f67e0c00aa41e079f1fc6d8cbebfaade4c231e
Preston Wallace <wallace.preston@gmail.com>
2023-11-28 06:25:41 -0800

    add proxy for socket connection

--- src/server/index.js
+++ src/server/index.js
@@ -1,30 +1,37 @@
-const path = require("path")
-require("dotenv").config({
-	path: path.resolve(__dirname, `../../.env`),
-})
+const path = require('path');
+require('dotenv').config({
+    path: path.resolve(__dirname, `../../.env`)
+});
 
-const express = require("express")
-const { createProxyMiddleware } = require("http-proxy-middleware")
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const PORT = process.env.PORT || 80
-const REACT_APP_API_URL = process.env.REACT_APP_API_URL || "http://localhost:3051"
+const PORT = process.env.PORT || 3050;
+const REACT_APP_API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3051';
 
-const app = express()
+const app = express();
 
 // Handle proxy to REACT_APP_API_URL
 const apiProxy = createProxyMiddleware({
-	target: REACT_APP_API_URL,
-})
-app.use("/api", apiProxy)
+    target: REACT_APP_API_URL
+});
+app.use('/api', apiProxy);
+
+const socketProxy = createProxyMiddleware({
+    target: REACT_APP_API_URL,
+    changeOrigin: true,
+    ws: true // enable WebSocket proxy
+});
+app.use('/socket.io', socketProxy);
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../../build")))
+app.use(express.static(path.resolve(__dirname, '../../build')));
 
 // All other GET requests not handled before will return our React app
-app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "../../build", "index.html"))
-})
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../../build', 'index.html'));
+});
 
 app.listen(PORT, () => {
-	console.log(`Server listening on ${PORT}`)
-})
+    console.log(`Server listening on ${PORT}`);
+});
