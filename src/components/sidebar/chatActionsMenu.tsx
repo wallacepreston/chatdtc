bf22f1567b18d82fc7f15daf79d14f49afb631fa
Preston Wallace <wallace.preston@gmail.com>
2023-12-10 00:12:16 -0800

    delete chat modal

--- src/components/sidebar/chatActionsMenu.tsx
+++ src/components/sidebar/chatActionsMenu.tsx
@@ -3,12 +3,13 @@ import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import { DeleteOutline, Edit, MoreHoriz } from '@mui/icons-material';
-import { ListItemIcon, ListItemText } from '@mui/material';
+import { Button, Grid, ListItemIcon, ListItemText, Stack, Typography } from '@mui/material';
 import theme from '../../theme';
 import useApi from '../../hooks/api';
 import { useStatus } from '../../contexts/status';
 import { useChats } from '../../contexts/chat';
 import { useNavigate } from 'react-router-dom';
+import WpModal from '../wpModal';
 
 interface ChatActionsProps {
     chatId: string;
@@ -17,6 +18,7 @@ interface ChatActionsProps {
 
 const ChatActionsMenu = ({ chatId, handleRename }: ChatActionsProps) => {
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [deleteModalOpen, setDeleteModalOpen] = React.useState(false);
     const { callApi } = useApi();
     const { setStatus } = useStatus();
     const { getChats } = useChats();
@@ -64,6 +66,16 @@ const ChatActionsMenu = ({ chatId, handleRename }: ChatActionsProps) => {
         handleClose();
     };
 
+    const handleOpenDeleteModal = () => {
+        handleClose();
+        setDeleteModalOpen(true);
+    };
+
+    const handleConfirmDelete = () => {
+        setDeleteModalOpen(false);
+        handleDelete();
+    };
+
     const options = [
         {
             title: 'Rename',
@@ -72,7 +84,7 @@ const ChatActionsMenu = ({ chatId, handleRename }: ChatActionsProps) => {
         },
         {
             title: 'Delete',
-            handleClick: () => handleDelete(),
+            handleClick: () => handleOpenDeleteModal(),
             icon: <DeleteOutline fontSize='small' />,
             color: theme.palette.error.main
         }
@@ -80,6 +92,32 @@ const ChatActionsMenu = ({ chatId, handleRename }: ChatActionsProps) => {
 
     return (
         <div>
+            <WpModal title='Delete Chat' open={deleteModalOpen} onClose={() => setDeleteModalOpen(false)}>
+                <Grid
+                    item
+                    xs={12}
+                    md={12}
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'flex-end'
+                    }}
+                >
+                    <Stack>
+                        <Typography>
+                            This will <b>delete</b> this chat. Are you sure you want to continue?
+                        </Typography>
+                        <br />
+                        <Stack direction='row' display='flex' justifyContent='space-evenly'>
+                            <Button variant='outlined' color='primary' onClick={() => setDeleteModalOpen(false)}>
+                                Cancel
+                            </Button>
+                            <Button variant='contained' color='error' onClick={handleConfirmDelete}>
+                                Delete
+                            </Button>
+                        </Stack>
+                    </Stack>
+                </Grid>
+            </WpModal>
             <IconButton
                 aria-label='more'
                 id='chat-actions-button'
