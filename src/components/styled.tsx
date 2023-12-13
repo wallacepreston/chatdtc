91eb86d0fa010213d293281229fdcd3b46ae46a0
Preston Wallace <wallace.preston@gmail.com>
2023-12-13 11:25:07 -0800

    increase modal width

--- src/components/styled.tsx
+++ src/components/styled.tsx
@@ -15,7 +15,7 @@ export const StyledModalBox = styled(Box)`
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    max-width: 400px;
+    max-width: 500px;
     background-color: #fff;
     box-shadow: 0px 0px 24px rgba(0, 0, 0, 0.15); /* Assuming '24' is the blur radius and the color is black with 15% opacity */
     padding: 16px; /* Assuming '4' is a spacing unit equivalent to 16px */
