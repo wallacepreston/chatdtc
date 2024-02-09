b7942b408a80537c3405d3487edff4108afc4d25
Preston Wallace <wallace.preston@gmail.com>
2024-02-09 10:55:42 -0800

    handle creating customer tag

--- src/constants/toolCalls.ts
+++ src/constants/toolCalls.ts
@@ -1,5 +1,6 @@
 export const TOOL_CALL_FUNCTION_MAP = {
     getCustomerTagsInCommerce7: 'Get Available Tags for Customers in Commerce7',
     textCustomersInRedchirp: 'Text Customers in Redchirp',
-    tagCustomersCommerce7: 'Tag Customers in Commerce7'
+    tagCustomersCommerce7: 'Tag Customers in Commerce7',
+    createCustomerTagInCommerce7: 'Create Customer Tag in Commerce7'
 } as const;
