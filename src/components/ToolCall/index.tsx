cd1d234f46ca88db1a679c5e2565594b2328032c
Preston Wallace <wallace.preston@gmail.com>
2024-02-19 10:48:10 -0800

    no modal necessary for tool calls

--- src/components/ToolCall/index.tsx
+++ src/components/ToolCall/index.tsx
@@ -1,22 +1,8 @@
-import {
-    Alert,
-    Box,
-    Button,
-    Checkbox,
-    Chip,
-    Divider,
-    FormControlLabel,
-    FormGroup,
-    Grid,
-    Typography,
-    Stack
-} from '@mui/material';
+import { Box, Button, Checkbox, Divider, FormControlLabel, FormGroup, Grid, Typography } from '@mui/material';
 import { ToolCall } from '../../contexts/chat';
 import { useStatus } from '../../contexts/status';
 import useApi from '../../hooks/api';
-import WpModal from '../wpModal';
 import { useEffect, useState } from 'react';
-import { Clear, Done } from '@mui/icons-material';
 import { TOOL_CALLS_THAT_REQUIRE_CONFIRMATION, TOOL_CALL_FUNCTION_MAP } from '../../constants/toolCalls';
 import { useThinking } from '../../contexts/thinking';
 import { renderToolCallDescription } from './helpers';
@@ -32,7 +18,6 @@ interface ToolCallProps {
 const ToolCalls = ({ toolCalls, runId, getMessages, chat_id }: ToolCallProps) => {
     const { callApi } = useApi();
     const { setStatus } = useStatus();
-    const [open, setOpen] = useState(false);
     const [toolCallsToSubmit, setToolCallsToSubmit] = useState<ToolCall[]>([]);
     const { addChatThinking } = useThinking();
 
@@ -52,20 +37,12 @@ const ToolCalls = ({ toolCalls, runId, getMessages, chat_id }: ToolCallProps) =>
     const allDeclined = toolCallsToSubmit.every(toolCall => toolCall.Status === 'declined');
 
     useEffect(() => {
-        setOpen(false);
-    }, [runId]);
-
-    useEffect(() => {
         const toolCallsDefaultedToDeclined = toolCallsToConfirm.map(toolCall => {
             return { ...toolCall, Status: 'declined' };
         });
         setToolCallsToSubmit(toolCallsDefaultedToDeclined);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [runId]);
-
-    const handleClose = () => {
-        setOpen(false);
-    };
 
     const handleToggleToolCall = (callId: string) => {
         const newToolCalls = toolCallsToSubmit.map(toolCall => {
@@ -135,79 +112,12 @@ const ToolCalls = ({ toolCalls, runId, getMessages, chat_id }: ToolCallProps) =>
                 message: 'Error saving actions.'
             });
         } finally {
-            handleClose();
             getMessages();
         }
     };
 
-    const warningAlert = (
-        <Box sx={{ p: 2 }}>
-            <Alert severity='warning'>
-                <b>NOTE:</b> These actions will be executed immediately.
-            </Alert>
-        </Box>
-    );
-
     return (
         <div>
-            <WpModal title='Requested Actions' open={open} onClose={handleClose}>
-                <Box sx={{ p: 2 }}>
-                    <Typography gutterBottom variant='body2'>
-                        The following actions will be taken:
-                    </Typography>
-                </Box>
-                <Box sx={{ p: 2 }}>
-                    <Stack spacing={1}>
-                        {toolCallsToSubmit.map(toolCall => {
-                            return (
-                                <Chip
-                                    key={toolCall.Call_OpenAI_id}
-                                    label={
-                                        TOOL_CALL_FUNCTION_MAP[
-                                            toolCall.FunctionName as keyof typeof TOOL_CALL_FUNCTION_MAP
-                                        ]
-                                    }
-                                    avatar={toolCall.Status === 'completed' ? <Done /> : <Clear />}
-                                    variant='outlined'
-                                    color={toolCall.Status === 'completed' ? 'success' : 'info'}
-                                />
-                            );
-                        })}
-                    </Stack>
-                </Box>
-                <Box sx={{ p: 2 }}>
-                    <Grid
-                        container
-                        spacing={2}
-                        sx={{
-                            p: 2
-                        }}
-                    >
-                        <Grid item xs={6}>
-                            <Button
-                                sx={{ textTransform: 'none' }}
-                                variant='contained'
-                                color='primary'
-                                onClick={() => handleToolCall()}
-                            >
-                                Approve
-                            </Button>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <Button
-                                sx={{ textTransform: 'none' }}
-                                variant='outlined'
-                                color='error'
-                                onClick={() => setOpen(false)}
-                            >
-                                Cancel
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </Box>
-                <Divider light />
-                <Box sx={{ p: 2 }}>{warningAlert}</Box>
-            </WpModal>
             <Grid
                 container
                 spacing={2}
@@ -264,7 +174,7 @@ const ToolCalls = ({ toolCalls, runId, getMessages, chat_id }: ToolCallProps) =>
                                     sx={{ textTransform: 'none' }}
                                     variant='contained'
                                     color='primary'
-                                    onClick={() => setOpen(true)}
+                                    onClick={() => handleToolCall()}
                                     disabled={allDeclined}
                                 >
                                     Approve
